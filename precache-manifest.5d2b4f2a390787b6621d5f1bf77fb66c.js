ab2c820553c57d1c974b04a04c9c4a72ee416057
Jaivrat12 <jaivratdas123456@gmail.com>
2020-06-30 21:57:11 +0530

    Updates

---
+++ precache-manifest.5d2b4f2a390787b6621d5f1bf77fb66c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c482e06ff261d2f1ebe9626543a3b17",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "695185bf1f194ab08ef0",
+    "url": "/first-react-app/static/css/main.ef3e2b69.chunk.css"
+  },
+  {
+    "revision": "b4caec94ae7a93b2df82",
+    "url": "/first-react-app/static/js/2.320d5ab8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.320d5ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695185bf1f194ab08ef0",
+    "url": "/first-react-app/static/js/main.59255997.chunk.js"
+  },
+  {
+    "revision": "6bd015cdd0228c321a7d",
+    "url": "/first-react-app/static/js/runtime-main.93f4e4a8.js"
+  }
+]);
